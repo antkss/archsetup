a8f4909da2911b0bb2886264d010e88e6d0ab7d2
antkss <88892713+antkss@users.noreply.github.com>
2023-09-25 23:13:42 +0700

    Update setuppacman.sh

--- setuppacman.sh
+++ setuppacman.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#install with aur libtiff5 
+#install with aur for wpsoffice
+#gay -S libtiff5 freetype2-ultimate5
 sudo pacman -S gst-libav gst-plugins-{base,good,bad,ugly} gstreamer-vaapi dnsmasq procps neofetch iptables hostapd xfce4-terminal libsass sassc git neovim nano sway zsh networkmanager waybar swayidle mpv mako grim slurp swaybg rofi alacritty xorg nodejs npm pipewire pipewire-media-session appmenu-gtk-module p7zip pulseaudio brightnessctl terminator noto-fonts-emoji pamixer wl-clipboard wayland-protocols nemo ast-firmware linux-firmware-qlogic wd719x-firmware linux-firmware intel-media-driver intel-hybrid-codec-driver papirus-icon-theme upd72020x-fw aic94xx-firmware auto-cpufreq gdm && systemctl enable NetworkManager && systemctl enable auto-cpufreq
